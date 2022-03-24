937c910b1c257a578e5ab4589db3fba009dc6827
ola-kanakri <ola.kanakri12@gmail.com>
2022-03-24 07:12:06 +0300

    23-Mar

---
+++ JavaScript Tasks/2nd Task_21-Mar/part1.js
@@ -0,0 +1,7 @@
+/* 1. Write a JavaScript program that accept two integers and
+ * display the larger. Go to the editor */
+/******* Start Your Code *********/
+  
+
+
+ /******* End Your Code ********* */
